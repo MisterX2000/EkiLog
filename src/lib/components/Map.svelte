10a5a531b5010ffa8e47e7ec1f3d9c2af5352db6
Mister_X <MisterX2000@users.noreply.github.com>
2026-06-22 23:38:34 +0200

    update dependencies and leaflet imports

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
+	import L, { Map as LeafletMap, Marker, Polyline } from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
 
 	interface Props {
 		depLat: number | null;
@@ -20,30 +22,12 @@
 	}: Props = $props();
 
 	let mapEl: HTMLDivElement;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	let leafletMap: any = null;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	let L: any;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	let depMarker: any = null;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	let arrMarker: any = null;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	let routeLine: any = null;
+	let leafletMap: LeafletMap | null = null;
+	let depMarker: Marker | null = null;
+	let arrMarker: Marker | null = null;
+	let routeLine: Polyline | null = null;
 
-	onMount(async () => {
-		// Dynamic import to prevent SSR issues
-		const leaflet = await import('leaflet');
-		L = leaflet.default ?? leaflet;
-
-		// Fix default marker icon paths broken by bundlers
-		delete L.Icon.Default.prototype._getIconUrl;
-		L.Icon.Default.mergeOptions({
-			iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-			iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-			shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
-		});
-
+	onMount(() => {
 		leafletMap = L.map(mapEl).setView([36.5, 138], 5);
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution:
@@ -59,7 +43,7 @@
 	});
 
 	function updateMarkers() {
-		if (!leafletMap || !L) return;
+		if (!leafletMap) return;
 
 		// Clean up
 		depMarker?.remove();
@@ -122,14 +106,5 @@
 		if (leafletMap) updateMarkers();
 	});
 </script>
-
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-		integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-		crossorigin="anonymous"
-	/>
-</svelte:head>
 
 <div bind:this={mapEl} class="h-full w-full rounded-2xl"></div>
